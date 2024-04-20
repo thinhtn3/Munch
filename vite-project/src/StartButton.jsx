8630080d1076ef5969409025b06fa9e71d7a4a4b
thinhtn3 <thinhttnguyen@gmail.com>
2024-04-20 16:44:47 -0700

    Added photo option

--- vite-project/src/StartButton.jsx
+++ vite-project/src/StartButton.jsx
@@ -1,14 +1,42 @@
-import "./StartButton.css"
+import "./StartButton.css";
+import React, { useState } from 'react';
+
 
 export default function StartButton() {
+  const [imagePreviewUrl, setImagePreviewUrl] = useState("");
 
-    const takePhoto = (e) => {
-        const file=e.target.files[0];
-        if (file) {
-            console.log(file)
-        }
+  const handleImageChange = (e) => {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      setImagePreviewUrl(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
     }
-    return (
-        <button onChangeclassName="takePhotoButton">Take Photo</button>
-    )
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="file"
+          accept="image/*"
+          capture="environment"
+          onChange={handleImageChange}
+        />
+        {imagePreviewUrl && (
+          <img
+            src={imagePreviewUrl}
+            alt="Preview"
+            style={{ width: "100%", height: "auto" }}
+          />
+        )}
+      </form>
+    </div>
+  );
 }
