1dd9fc63c176641a4c4c17ec5808a0789b07c1d1
thinhtn3 <thinhttnguyen@gmail.com>
2024-04-20 14:13:07 -0700

    json'd business array of objects

--- API/index.js
+++ API/index.js
@@ -1,10 +1,13 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
 const fs = require("fs");
-
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-
 const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+const express = require("express");
+const axios = require("axios");
+const app = express();
+let jsonData = "";
+let location = "";
 
 const run = async () => {
   try {
@@ -28,14 +31,42 @@ const run = async () => {
     const response = await result.response;
     const text = await response.text();
     const cleanText = text.replace(/```json|```/g, "").trim();
-    // Log the response
-    // console.log(result.response.text());
-    const jsonData = JSON.parse(cleanText)
-    console.log(jsonData.food_name)
-    console.log(jsonData.cuisine_type)
+    jsonData = JSON.parse(cleanText);
+    return jsonData;
   } catch (error) {
     console.error("An error occurred:", error);
   }
 };
 
-run();
+app.get("/findRestaurant", async (req, res) => {
+  jsonData = await run();
+  const config = {
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_API_KEY}`, // Replace 'YOUR_ACCESS_TOKEN' with your actual Yelp API token
+      "Content-Type": "application/json",
+    },
+  };
+
+  const response = await axios.get(
+    `https://api.yelp.com/v3/businesses/search?location=westminsterCA&categories=${jsonData.cuisine_type.toLowerCase()}&sort_by=review_count`,
+    config
+  );
+  const businesses = response.data.businesses.map((business) => {
+    return {
+      id: business.id,
+      name: business.name,
+      location: business.location.address1,
+      reviews: business.review_count,
+      rating: business.rating,
+      url: business.url,
+      is_closed: business.is_closed,
+      image_url: business.image_url,
+      phone_number: business.display_phone,
+    };
+  });
+  res.json(businesses);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on ${process.env.PORT}`);
+});
